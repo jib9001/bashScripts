2a6b07a2e33c46184af260161f76d64660f68478
Connor Tellep <jib11@live.com>
2023-04-26 13:35:41 -0400

    added basic install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo chmod +x aptFix && sudo cp aptFix /bin
+sudo chmod +x dpkgFix && sudo cp dpkgFix /bin
+sudo chmod +x gdmenuGenerator && sudo cp gdmenuGenerator /bin
+sudo chmod +x protonUpdate && sudo cp protonUpdate /bin
+sudo chmod +x update && sudo cp update /bin
